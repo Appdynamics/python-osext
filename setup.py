2744aef61b3c932118c3399a8921fb654cdbb36f
Andrew Udvare <audvare@gmail.com>
2013-11-18 17:38:55 -0800

    Include *.rst; bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='OSExtension',
-    version='0.1.0',
+    version='0.1.1',
     author='Andrew Udvare',
     author_email='audvare@gmail.com',
     packages=['osext'],
