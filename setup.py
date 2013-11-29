80bb5d7b6e5ff3c8491c79f9ab126f79f517bdbd
Andrew Udvare <audvare@gmail.com>
2013-11-28 19:23:22 -0800

    Release v0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='OSExtension',
-    version='0.1.1',
+    version='0.1.2',
     author='Andrew Udvare',
     author_email='audvare@gmail.com',
     packages=['osext'],
